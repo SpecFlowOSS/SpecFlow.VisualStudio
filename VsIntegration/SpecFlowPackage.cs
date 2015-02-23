283ee931a29709fdc9ca875e4130427efaff2b60
Gaspar Nagy <gaspar.nagy@gmail.com>
2015-02-23 13:09:47 +0100

    merge changes of v1.9.3

--- VsIntegration/SpecFlowPackage.cs
+++ VsIntegration/SpecFlowPackage.cs
@@ -30,7 +30,7 @@ namespace TechTalk.SpecFlow.VsIntegration
     [PackageRegistration(UseManagedResourcesOnly = true)]
     // This attribute is used to register the informations needed to show the this package
     // in the Help/About dialog of Visual Studio.
-    [InstalledProductRegistration("#110", "#112", "1.9.2", IconResourceID = 400)]
+    [InstalledProductRegistration("#110", "#112", "1.9.3", IconResourceID = 400)]
     [ProvideOptionPage(typeof(OptionsPageGeneral), IntegrationOptionsProvider.SPECFLOW_OPTIONS_CATEGORY, IntegrationOptionsProvider.SPECFLOW_GENERAL_OPTIONS_PAGE, 121, 122, true)]
     [ProvideProfile(typeof(OptionsPageGeneral), IntegrationOptionsProvider.SPECFLOW_OPTIONS_CATEGORY, IntegrationOptionsProvider.SPECFLOW_GENERAL_OPTIONS_PAGE, 121, 123, true, DescriptionResourceID = 121)]
     [Guid(GuidList.guidSpecFlowPkgString)]
