cd005f0b0a88ec23bda54c9e48662f35e4c64f8a
Linda van Zyl <linda.vanzyl@xero.com>
2015-05-11 21:45:00 +1200

    Updated ResharperRunnerGateway/Loader to support any version of Resharper - assuming that the only difference is the Run/Debug Context commands.

--- VsIntegration/TestRunner/ReSharper6TestRunnerGateway.cs
+++ VsIntegration/TestRunner/ReSharper6TestRunnerGateway.cs
@@ -8,30 +8,55 @@ namespace TechTalk.SpecFlow.VsIntegration.TestRunner
 {
     public class ReSharper6GatewayLoader : AutoTestRunnerGatewayLoader
     {
-        public ReSharper6GatewayLoader() : base (TestRunnerTool.ReSharper)
+        public ReSharper6GatewayLoader()
+            : base(TestRunnerTool.ReSharper)
         {
         }
 
         public override bool CanUse(Project project)
         {
+            return GetResharperVersion() != -1;
+        }
+
+        public static int GetResharperVersion()
+        {
             var reSharperAssembly = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(a => a.GetName().Name == "JetBrains.ReSharper.UnitTestFramework");
-            return reSharperAssembly != null && reSharperAssembly.GetName().Version.Major <= 5;
+            return reSharperAssembly != null ? reSharperAssembly.GetName().Version.Major : -1;
         }
     }
 
     public class ReSharper6TestRunnerGateway : CommandBasedTestRunnerGateway
     {
+        private readonly int _currentVersion;
+
         protected override string GetRunInCurrentContextCommand(bool debug)
         {
-            if (debug)
-                return "ReSharper.Reharper_ReSharperUnitTestDebugContext";
+            string commandFormat;
+            if (_currentVersion < 6)
+            {
+                commandFormat = "ReSharper.ReSharper_UnitTest_Context{0}";
+            }
+            else if (_currentVersion < 9)
+            {
+                commandFormat = "ReSharper.ReSharper_ReSharper_UnitTest_{0}Context";
+            }
+            else
+            {
+                commandFormat = "ReSharper.ReSharper_ReSharperUnitTest{0}Context";
+            }
 
-            return "ReSharper.Reharper_ReSharperUnitTestRunContext";
+            if (debug)
+            {
+                return string.Format(commandFormat, "Debug");
+            }
+            return string.Format(commandFormat, "Run");
+
         }
 
         public ReSharper6TestRunnerGateway(DTE dte, IIdeTracer tracer)
             : base(dte, tracer)
         {
+            _currentVersion = ReSharper6GatewayLoader.GetResharperVersion();
         }
     }
 }
