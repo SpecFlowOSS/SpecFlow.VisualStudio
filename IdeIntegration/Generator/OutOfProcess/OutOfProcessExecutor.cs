0ee837e0c7ffca9d7621a9f7e8f39b1d182047f2
Andreas Willich <andreas.willich@techtalk.at>
2018-03-30 10:30:29 +0200

    use full path to exe

--- IdeIntegration/Generator/OutOfProcess/OutOfProcessExecutor.cs
+++ IdeIntegration/Generator/OutOfProcess/OutOfProcessExecutor.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 using CommandLine;
 using TechTalk.SpecFlow.RemoteAppDomain;
 using TechTalk.SpecFlow.VisualStudio.CodeBehindGenerator.Parameters;
@@ -13,12 +8,15 @@ namespace TechTalk.SpecFlow.IdeIntegration.Generator.OutOfProcess
     class OutOfProcessExecutor
     {
         private readonly Info _info;
+        private readonly string _fullPathToExe;
         private const string ExeName = "TechTalk.SpecFlow.VisualStudio.CodeBehindGenerator.exe";
 
 
         public OutOfProcessExecutor(Info info)
         {
             _info = info;
+            string currentDirectory = Path.GetDirectoryName(GetType().Assembly.Location);
+            _fullPathToExe = Path.Combine(currentDirectory, ExeName);
         }
 
         public Result Execute(CommonParameters commonParameters)
@@ -27,7 +25,7 @@ namespace TechTalk.SpecFlow.IdeIntegration.Generator.OutOfProcess
 
             var processHelper = new ProcessHelper();
 
-            int exitCode = processHelper.RunProcess(_info.GeneratorFolder, ExeName, commandLineParameters);
+            int exitCode = processHelper.RunProcess(_info.GeneratorFolder, _fullPathToExe, commandLineParameters);
 
             var outputFileContent = processHelper.ConsoleOutput;
 
