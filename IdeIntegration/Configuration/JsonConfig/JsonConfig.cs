fe9d420d0de3053543d03212b11aa08708379fb3
Balázs Epresi <Balazs.Epresi@techtalk.at>
2018-09-07 11:47:12 +0200

    Configuration (specflow.json) should not contain a specflow node anymore

--- IdeIntegration/Configuration/JsonConfig/JsonConfig.cs
+++ IdeIntegration/Configuration/JsonConfig/JsonConfig.cs
@@ -1,16 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TechTalk.SpecFlow.IdeIntegration.Configuration.JsonConfig
 {
     public class JsonConfig
     {
-        //[JsonProperty(PropertyName = "specflow", NullValueHandling = NullValueHandling.Ignore)]
-        [DataMember(Name = "specflow")]
-        public SpecFlowElement SpecFlow { get; set; }
+        //[JsonProperty("language", NullValueHandling = NullValueHandling.Ignore)]
+        [DataMember(Name = "language")]
+        public LanguageElement Language { get; set; }
+
+        [DataMember(Name = "bindingCulture")]
+        public BindingCultureElement BindingCulture { get; set; }
+
+        //[JsonProperty("stepAssemblies", NullValueHandling = NullValueHandling.Ignore)]
+        [DataMember(Name = "stepAssemblies")]
+        public List<StepAssemblyElement> StepAssemblies { get; set; }
+
+        [DataMember(Name = "trace")]
+        public TraceElement Trace { get; set; }
+
+        //[JsonProperty("generator", NullValueHandling = NullValueHandling.Ignore)]
+        [DataMember(Name = "generator")]
+        public GeneratorElement Generator { get; set; }
+
+        [DataMember(Name = "unitTestProvider")]
+        public UnitTestProviderElement UnitTestProvider { get; set; }
     }
 }
