736754b373308be8a74c4abc3671e9b0e2eb5236
David Eiwen <david-e-winlive@live.at>
2018-10-01 11:40:38 +0200

    fix variable for artifacts directory

--- publish.myget.vs2017.ps1
+++ publish.myget.vs2017.ps1
@@ -1,2 +1,2 @@
 
-& curl -X POST --verbose --data-binary \@"$(System.ArtifactsDirectory)\s\VsIntegration\bin\Release\TechTalk.SpecFlow.VsIntegration.2015.vsix" -H "X-NuGet-ApiKey: $(MyGetApiKey)" "$(MyGetVsixFeed)/upload"
+& curl -X POST --verbose --data-binary \@"$(SYSTEM_ARTIFACTSDIRECTORY)\s\VsIntegration\bin\Release\TechTalk.SpecFlow.VsIntegration.2015.vsix" -H "X-NuGet-ApiKey: $(MyGetApiKey)" "$(MyGetVsixFeed)/upload"
