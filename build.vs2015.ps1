c922daee7d46d3f0af45137e0cbd0056297d6cf2
David Eiwen <david-e-winlive@live.at>
2018-10-01 15:51:13 +0200

    Revert explicit path on msbuild

--- build.vs2015.ps1
+++ build.vs2015.ps1
@@ -2,23 +2,23 @@ param (
  [string]$Configuration = "Debug"
 )
 
-$msbuildPath = "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe"
+#$msbuildPath = "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe"
+$msbuildPath = "msbuild"
 
-
-# if ([Environment]::OSVersion.Platform -eq "Win32NT"){
-#   $vswherePath = [System.Environment]::ExpandEnvironmentVariables("%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe")
-#   $vswhereParameters = @("-latest", "-products", "*", "-requires", "Microsoft.Component.MSBuild",  "-property", "installationPath")
+if ([Environment]::OSVersion.Platform -eq "Win32NT"){
+  $vswherePath = [System.Environment]::ExpandEnvironmentVariables("%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe")
+  $vswhereParameters = @("-latest", "-products", "*", "-requires", "Microsoft.Component.MSBuild",  "-property", "installationPath")
   
-#   $vsPath = & $vswherePath $vswhereParameters
+  $vsPath = & $vswherePath $vswhereParameters
   
-#   Write-Host $path
+  Write-Host $path
   
-#   if ($vsPath) {
-#     $msbuildPath = join-path $vsPath 'MSBuild\15.0\Bin\MSBuild.exe'
-#   }
+  if ($vsPath) {
+    $msbuildPath = join-path $vsPath 'MSBuild\15.0\Bin\MSBuild.exe'
+  }
   
-#   Write-Host $msbuildPath
-# }
+  Write-Host $msbuildPath
+}
 
 & nuget restore ./SpecFlow.VisualStudio.2015.sln
 & $msbuildPath /Restore ./SpecFlow.VisualStudio.2015.sln /property:Configuration=$Configuration /binaryLogger:msbuild.$Configuration.binlog /nodeReuse:false
