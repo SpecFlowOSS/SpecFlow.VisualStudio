dcc6fdd600073cfde5bcba9e0619ef371eb6a4e5
David Eiwen <david-e-winlive@live.at>
2018-10-01 14:49:58 +0200

    add quotation marks to parameters

--- publish.myget.vs2015.ps1
+++ publish.myget.vs2015.ps1
@@ -5,6 +5,6 @@ $MyGetVsixFeed = $Env:MyGetVsixFeed;
 
 $pathToExtension = "$SystemArtifactsDirectory\SpecFlow.VisualStudio.VS2015\s\VsIntegration\bin\Release\TechTalk.SpecFlow.VsIntegration.2015.vsix";
 
-$extensionFileContent = [System.IO.File]::ReadAllBytes($pathToExtension);
+$extensionFileContent = [System.IO.File]::ReadAllBytes("$pathToExtension");
 
 & Invoke-WebRequest -Uri "$MyGetVsixFeed/upload" -Method 'POST' -Body $extensionFileContent -Headers @{"X-NuGet-ApiKey"="$MyGetApiKey"}
--- publish.myget.vs2017.ps1
+++ publish.myget.vs2017.ps1
@@ -5,6 +5,6 @@ $MyGetVsixFeed = $Env:MyGetVsixFeed;
 
 $pathToExtension = "$SystemArtifactsDirectory\SpecFlow.VisualStudio.VS2017\s\VsIntegration\bin\Release\TechTalk.SpecFlow.VsIntegration.2017.vsix";
 
-$extensionFileContent = [System.IO.File]::ReadAllBytes($pathToExtension);
+$extensionFileContent = [System.IO.File]::ReadAllBytes("$pathToExtension");
 
 & Invoke-WebRequest -Uri "$MyGetVsixFeed/upload" -Method 'POST' -Body $extensionFileContent -Headers @{"X-NuGet-ApiKey"="$MyGetApiKey"}
