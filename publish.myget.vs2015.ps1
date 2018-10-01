853745af90f5a8d51ecf1fb14dc4ce020c1ecf84
David Eiwen <david-e-winlive@live.at>
2018-10-01 12:41:37 +0200

    use environment variables correctly

--- publish.myget.vs2015.ps1
+++ publish.myget.vs2015.ps1
@@ -1,2 +1,6 @@
 
-& curl -X POST --verbose --data-binary \@".\VsIntegration\bin\Release\TechTalk.SpecFlow.VsIntegration.2017.vsix" -H "X-NuGet-ApiKey: $(MyGetApiKey)" "$(MyGetVsixFeed)/upload"
+$SystemArtifactsDirectory = $Env:SYSTEM_ARTIFACTSDIRECTORY;
+$MyGetApiKey = $Env:MyGetApiKey;
+$MyGetVsixFeed = $Env:MyGetVsixFeed;
+
+& curl -X POST --verbose --data-binary \@"$SystemArtifactsDirectory\VsIntegration\bin\Release\TechTalk.SpecFlow.VsIntegration.2017.vsix" -H "X-NuGet-ApiKey: $MyGetApiKey" "$MyGetVsixFeed/upload"
