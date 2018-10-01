3b0c6dc95b2dcdee96220e4139d33d15db4e703d
David Eiwen <david-e-winlive@live.at>
2018-10-01 09:23:20 +0200

    add build configurations for azure pipelines

---
+++ build.vs2015.ps1
@@ -0,0 +1,22 @@
+param (
+ [string]$Configuration = "Debug"
+)
+
+$msbuildPath = "msbuild"
+
+if ([Environment]::OSVersion.Platform -eq "Win32NT"){
+  $vswherePath = [System.Environment]::ExpandEnvironmentVariables("%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe")
+  $vswhereParameters = @("-latest", "-products", "*", "-requires", "Microsoft.Component.MSBuild",  "-property", "installationPath")
+  
+  $vsPath = & $vswherePath $vswhereParameters
+  
+  Write-Host $path
+  
+  if ($vsPath) {
+    $msbuildPath = join-path $vsPath 'MSBuild\15.0\Bin\MSBuild.exe'
+  }
+  
+  Write-Host $msbuildPath
+}
+
+& $msbuildPath /Restore ./.SpecFlow.VisualStudio.2015.sln /property:Configuration=$Configuration /binaryLogger:msbuild.$Configuration.binlog /nodeReuse:false
