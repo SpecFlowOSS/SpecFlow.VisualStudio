9cb0e514c245c5cfce64ad65e5abc54f5ed22990
David Eiwen <david-e-winlive@live.at>
2018-10-01 13:01:08 +0200

    use dictionary syntax for additional headers

--- publish.myget.vs2017.ps1
+++ publish.myget.vs2017.ps1
@@ -3,4 +3,4 @@ $SystemArtifactsDirectory = $Env:SYSTEM_ARTIFACTSDIRECTORY;
 $MyGetApiKey = $Env:MyGetApiKey;
 $MyGetVsixFeed = $Env:MyGetVsixFeed;
 
-& curl -X POST --verbose --data-binary \@"$SystemArtifactsDirectory\VsIntegration\bin\Release\TechTalk.SpecFlow.VsIntegration.2015.vsix" -H "X-NuGet-ApiKey: $MyGetApiKey" "$MyGetVsixFeed/upload"
+& curl -X POST --verbose --data-binary \@"$SystemArtifactsDirectory\VsIntegration\bin\Release\TechTalk.SpecFlow.VsIntegration.2015.vsix" -H @{"X-NuGet-ApiKey"="$MyGetApiKey"} "$MyGetVsixFeed/upload"
