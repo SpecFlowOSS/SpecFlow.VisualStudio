1490601c59dbc60f8b87e68df577caef3f81e0e1
David Eiwen <david-e-winlive@live.at>
2018-10-01 13:39:49 +0200

    remove -x parameter

--- publish.myget.vs2015.ps1
+++ publish.myget.vs2015.ps1
@@ -3,4 +3,4 @@ $SystemArtifactsDirectory = $Env:SYSTEM_ARTIFACTSDIRECTORY;
 $MyGetApiKey = $Env:MyGetApiKey;
 $MyGetVsixFeed = $Env:MyGetVsixFeed;
 
-& curl -X POST --verbose --data-binary \@"$SystemArtifactsDirectory\VsIntegration\bin\Release\TechTalk.SpecFlow.VsIntegration.2017.vsix" -H @{"X-NuGet-ApiKey"="$MyGetApiKey"} "$MyGetVsixFeed/upload"
+& curl POST --verbose --data-binary \@"$SystemArtifactsDirectory\VsIntegration\bin\Release\TechTalk.SpecFlow.VsIntegration.2017.vsix" -H @{"X-NuGet-ApiKey"="$MyGetApiKey"} "$MyGetVsixFeed/upload"
