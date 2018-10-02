e65700d4e15ab8432a1db2aa9ff732b7e52309dd
David Eiwen <david-e-winlive@live.at>
2018-10-02 09:35:44 +0200

    Add environment variable for visual studio version

--- build.ps1
+++ build.ps1
@@ -1,33 +1,37 @@
 param (
  [string]$Configuration = "Debug",
- [string]$ExtensionVisualStudioVersion = "2017",
+ [string]$VisualStudioVersion = "",
  [string]$binaryLoggerSwitch = "/binaryLogger"
 )
 
 $msbuildPath = "msbuild"
+
+$VisualStudioVersion =
+  if (![System.String]::IsNullOrWhiteSpace($VisualStudioVersion)) { $VisualStudioVersion }
+  else { $Env:VisualStudioVersion };
 
 if (![System.String]::IsNullOrEmpty($Env:MSBuild))
 {
   $msbuildPath = join-path $Env:MSBuild 'msbuild.exe';
   Write-Host "Using msbuild from environment variable";
 }
-elseif ($ExtensionVisualStudioVersion -eq "2017")
+elseif ($VisualStudioVersion -eq "2017")
 {
   $msbuildPath = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe";
   $binaryLoggerSwitch = "/binaryLogger:msbuild.$Configuration.binlog";
 }
-elseif ($ExtensionVisualStudioVersion -eq "2015")
+elseif ($VisualStudioVersion -eq "2015")
 {
   $msbuildPath = "C:\Program Files (x86)\MSBuild\14.0\bin\MSBuild.exe";
   $binaryLoggerSwitch = "";
 }
 else
 {
-  throw [System.NotSupportedException]::new("The Visual Studio version $ExtensionVisualStudioVersion is not supported.");
+  throw [System.NotSupportedException]::new("The Visual Studio version $VisualStudioVersion is not supported.");
 }
 
 Write-Host "MSBuild path: $msbuildPath"
 
 
-& nuget restore "./SpecFlow.VisualStudio.$ExtensionVisualStudioVersion.sln"
-& $msbuildPath "./SpecFlow.VisualStudio.$ExtensionVisualStudioVersion.sln" $binaryLoggerSwitch /property:Configuration=$Configuration  /nodeReuse:false
+& nuget restore "./SpecFlow.VisualStudio.$VisualStudioVersion.sln"
+& $msbuildPath "./SpecFlow.VisualStudio.$VisualStudioVersion.sln" $binaryLoggerSwitch /property:Configuration=$Configuration  /nodeReuse:false
