2577f4bfc1912594c4ce96d8771054757f73fa95
Andreas Willich <andreas.willich@techtalk.at>
2019-01-07 11:15:17 +0100

    handle exchange path without ending backslash

--- IdeIntegration/Generator/OutOfProcess/OutOfProcessExecutor.cs
+++ IdeIntegration/Generator/OutOfProcess/OutOfProcessExecutor.cs
@@ -34,9 +34,10 @@ namespace TechTalk.SpecFlow.IdeIntegration.Generator.OutOfProcess
 
         public Result Execute(CommonParameters commonParameters, bool transferViaFile)
         {
-            commonParameters.OutputDirectory = Path.GetDirectoryName(String.IsNullOrWhiteSpace(_integrationOptions.CodeBehindFileGeneratorExchangePath)
-                    ? Path.GetTempPath()
-                    : _integrationOptions.CodeBehindFileGeneratorExchangePath);
+            var exchangePath = String.IsNullOrWhiteSpace(_integrationOptions.CodeBehindFileGeneratorExchangePath)
+                ? Path.GetTempPath()
+                : _integrationOptions.CodeBehindFileGeneratorExchangePath;
+            commonParameters.OutputDirectory = Path.GetDirectoryName(PathAddBackslash(exchangePath));
 
 
             string commandLineParameters = CommandLine.Parser.Default.FormatCommandLine(commonParameters);
@@ -118,6 +119,43 @@ namespace TechTalk.SpecFlow.IdeIntegration.Generator.OutOfProcess
             }
 
             return output.ToString();
+        }
+
+
+        string PathAddBackslash(string path)
+        {
+            // They're always one character but EndsWith is shorter than
+            // array style access to last path character. Change this
+            // if performance are a (measured) issue.
+            string separator1 = Path.DirectorySeparatorChar.ToString();
+            string separator2 = Path.AltDirectorySeparatorChar.ToString();
+
+            // Trailing white spaces are always ignored but folders may have
+            // leading spaces. It's unusual but it may happen. If it's an issue
+            // then just replace TrimEnd() with Trim(). Tnx Paul Groke to point this out.
+            path = path.TrimEnd();
+
+            // Argument is always a directory name then if there is one
+            // of allowed separators then I have nothing to do.
+            if (path.EndsWith(separator1) || path.EndsWith(separator2))
+                return path;
+
+            // If there is the "alt" separator then I add a trailing one.
+            // Note that URI format (file://drive:\path\filename.ext) is
+            // not supported in most .NET I/O functions then we don't support it
+            // here too. If you have to then simply revert this check:
+            // if (path.Contains(separator1))
+            //     return path + separator1;
+            //
+            // return path + separator2;
+            if (path.Contains(separator2))
+                return path + separator2;
+
+            // If there is not an "alt" separator I add a "normal" one.
+            // It means path may be with normal one or it has not any separator
+            // (for example if it's just a directory name). In this case I
+            // default to normal as users expect.
+            return path + separator1;
         }
 
     }
