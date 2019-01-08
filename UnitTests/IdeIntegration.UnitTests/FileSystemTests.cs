e4c653972483e4565953d3cfc0db504b5a9d6f76
David Eiwen <david-e-winlive@live.at>
2019-01-08 10:54:29 +0100

    collapse all new tests into a single theory

--- UnitTests/IdeIntegration.UnitTests/FileSystemTests.cs
+++ UnitTests/IdeIntegration.UnitTests/FileSystemTests.cs
@@ -1,5 +1,4 @@
-﻿using System.IO;
-using FluentAssertions;
+﻿using FluentAssertions;
 using NUnit.Framework;
 using TechTalk.SpecFlow.IdeIntegration.Services;
 
@@ -7,87 +6,15 @@ namespace TechTalk.SpecFlow.IdeIntegration.UnitTests
 {
     public class FileSystemTests
     {
-        [Test]
-        public void AppendDirectorySeparatorIfNotPresent_PathWithAltSeparatorAtEnd_ShouldReturnSamePath()
+        [TestCase(@"C:/PathToDirectory/", @"C:/PathToDirectory/")]
+        [TestCase(@"C:/PathToDirectory", @"C:/PathToDirectory/")]
+        [TestCase(@"C:\PathToDirectory\", @"C:\PathToDirectory\")]
+        [TestCase(@"C:\PathToDirectory", @"C:\PathToDirectory\")]
+        [TestCase(@"C:\PathToDirectory      ", @"C:\PathToDirectory\")]
+        [TestCase(@"PathToDirectory", @"PathToDirectory\")]
+        public void AppendDirectorySeparatorIfNotPresent_Path_ShouldReturnCorrectPath(string inputPath, string expectedPath)
         {
             // ARRANGE
-            string inputPath = @"C:" + Path.AltDirectorySeparatorChar + "PathToDirectory" + Path.AltDirectorySeparatorChar;
-            string expectedPath = @"C:" + Path.AltDirectorySeparatorChar + "PathToDirectory" + Path.AltDirectorySeparatorChar;
-            var fileSystem = new FileSystem();
-
-            // ACT
-            string actualPath = fileSystem.AppendDirectorySeparatorIfNotPresent(inputPath);
-
-            // ASSERT
-            actualPath.Should().Be(expectedPath);
-        }
-
-        [Test]
-        public void AppendDirectorySeparatorIfNotPresent_PathWithSeparatorAtEnd_ShouldReturnSamePath()
-        {
-            // ARRANGE
-            string inputPath = @"C:" + Path.DirectorySeparatorChar + "PathToDirectory" + Path.DirectorySeparatorChar;
-            string expectedPath = @"C:" + Path.DirectorySeparatorChar + "PathToDirectory" + Path.DirectorySeparatorChar;
-            var fileSystem = new FileSystem();
-
-            // ACT
-            string actualPath = fileSystem.AppendDirectorySeparatorIfNotPresent(inputPath);
-
-            // ASSERT
-            actualPath.Should().Be(expectedPath);
-        }
-
-        [Test]
-        public void AppendDirectorySeparatorIfNotPresent_PathWithAltSeparator_ShouldReturnPathWithAltSeparatorAtEnd()
-        {
-            // ARRANGE
-            string inputPath = @"C:" + Path.AltDirectorySeparatorChar + "PathToDirectory";
-            string expectedPath = @"C:" + Path.AltDirectorySeparatorChar + "PathToDirectory" + Path.AltDirectorySeparatorChar;
-            var fileSystem = new FileSystem();
-
-            // ACT
-            string actualPath = fileSystem.AppendDirectorySeparatorIfNotPresent(inputPath);
-
-            // ASSERT
-            actualPath.Should().Be(expectedPath);
-        }
-
-        [Test]
-        public void AppendDirectorySeparatorIfNotPresent_PathWithSeparator_ShouldReturnPathWithSeparatorAtEnd()
-        {
-            // ARRANGE
-            string inputPath = @"C:" + Path.DirectorySeparatorChar + "PathToDirectory";
-            string expectedPath = @"C:" + Path.DirectorySeparatorChar + "PathToDirectory" + Path.DirectorySeparatorChar;
-            var fileSystem = new FileSystem();
-
-            // ACT
-            string actualPath = fileSystem.AppendDirectorySeparatorIfNotPresent(inputPath);
-
-            // ASSERT
-            actualPath.Should().Be(expectedPath);
-        }
-
-        [Test]
-        public void AppendDirectorySeparatorIfNotPresent_PathWithNoSeparator_ShouldReturnPathWithSeparatorAtEnd()
-        {
-            // ARRANGE
-            string inputPath = @"PathToDirectory";
-            string expectedPath = @"PathToDirectory" + Path.DirectorySeparatorChar;
-            var fileSystem = new FileSystem();
-
-            // ACT
-            string actualPath = fileSystem.AppendDirectorySeparatorIfNotPresent(inputPath);
-
-            // ASSERT
-            actualPath.Should().Be(expectedPath);
-        }
-
-        [Test]
-        public void AppendDirectorySeparatorIfNotPresent_PathWithSeparator_ShouldIgnoreWhitespaceAtEnd()
-        {
-            // ARRANGE
-            string inputPath = @"C:" + Path.DirectorySeparatorChar + "PathToDirectory        ";
-            string expectedPath = @"C:" + Path.DirectorySeparatorChar + "PathToDirectory" + Path.DirectorySeparatorChar;
             var fileSystem = new FileSystem();
 
             // ACT
