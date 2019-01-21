22725df62e1b3e6728a4ad19e7d9cfb0f408ba2c
slig3 <45394323+slig3@users.noreply.github.com>
2019-01-21 04:42:12 -0500

    Add RenameCommand (#39)
    Be able to select a step in a .feature file and rename it using Visual Studio rename shortcut.
    The user is prompted to modify the regex in the binding attribute of the step definition.
    Then, the modification on the regex is applied on all the steps that are bound to this method.
    The values specified in the step names are conserved after the name change.

---
+++ VsIntegration/StepSuggestions/StepInstanceWithProjectScope.cs
@@ -0,0 +1,17 @@
+﻿using TechTalk.SpecFlow.Bindings;
+using TechTalk.SpecFlow.VsIntegration.LanguageService;
+
+namespace TechTalk.SpecFlow.VsIntegration.StepSuggestions
+{
+    internal class StepInstanceWithProjectScope
+    {
+        public StepInstance StepInstance { get; private set; }
+        public VsProjectScope ProjectScope { get; private set; }
+
+        public StepInstanceWithProjectScope(StepInstance stepInstance, VsProjectScope projectScope)
+        {
+            StepInstance = stepInstance;
+            ProjectScope = projectScope;
+        }
+    }
+}
