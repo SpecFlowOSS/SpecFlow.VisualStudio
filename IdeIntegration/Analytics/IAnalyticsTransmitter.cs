ff3c7c0baf970488eb5169684a62e31e2d901c6d
David Eiwen <david-e-winlive@live.at>
2019-03-08 15:38:00 +0100

    add IAnalyticsTransmitter and events to IdeIntegration

---
+++ IdeIntegration/Analytics/IAnalyticsTransmitter.cs
@@ -0,0 +1,7 @@
+﻿namespace TechTalk.SpecFlow.IdeIntegration.Analytics
+{
+    public interface IAnalyticsTransmitter
+    {
+        void TransmitLogonEvent(string ide, string ideVersion, string extensionVersion);
+    }
+}
