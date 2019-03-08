45c28792b0b59aea561eea7c92c154c08b6e879c
David Eiwen <david-e-winlive@live.at>
2019-03-08 15:41:36 +0100

    add analytics transmitter and console sink

---
+++ VsIntegration/Analytics/AnalyticsTransmitter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using TechTalk.SpecFlow.IdeIntegration.Analytics;
+
+namespace TechTalk.SpecFlow.VsIntegration.Analytics
+{
+    public class AnalyticsTransmitter : IAnalyticsTransmitter
+    {
+        private readonly IUserUniqueIdStore _userUniqueIdStore;
+        private readonly IEnableAnalyticsChecker _enableAnalyticsChecker;
+        private readonly IAnalyticsTransmitterSink _analyticsTransmitterSink;
+
+        public AnalyticsTransmitter(IUserUniqueIdStore userUniqueIdStore, IEnableAnalyticsChecker enableAnalyticsChecker, IAnalyticsTransmitterSink analyticsTransmitterSink)
+        {
+            _userUniqueIdStore = userUniqueIdStore;
+            _enableAnalyticsChecker = enableAnalyticsChecker;
+            _analyticsTransmitterSink = analyticsTransmitterSink;
+        }
+
+        public void TransmitLogonEvent(string ide, string ideVersion, string extensionVersion)
+        {
+            if (!_enableAnalyticsChecker.IsEnabled())
+            {
+                return;
+            }
+            
+            var userUniqueId = _userUniqueIdStore.Get();
+            var logonAnalyticsEvent = new LogonAnalyticsEvent(DateTime.UtcNow, userUniqueId, ide, ideVersion, extensionVersion);
+            _analyticsTransmitterSink.TransmitEvent(logonAnalyticsEvent);
+        }
+    }
+}
---
+++ VsIntegration/Analytics/ConsoleAnalyticsTransmitterSink.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using TechTalk.SpecFlow.IdeIntegration.Analytics;
+
+namespace TechTalk.SpecFlow.VsIntegration.Analytics
+{
+    public class ConsoleAnalyticsTransmitterSink : IAnalyticsTransmitterSink
+    {
+        private readonly IEnableAnalyticsChecker _enableAnalyticsChecker;
+
+        public ConsoleAnalyticsTransmitterSink(IEnableAnalyticsChecker enableAnalyticsChecker)
+        {
+            _enableAnalyticsChecker = enableAnalyticsChecker;
+        }
+
+        public void TransmitEvent(IAnalyticsEvent analyticsEvent)
+        {
+            if (!_enableAnalyticsChecker.IsEnabled())
+            {
+                throw new InvalidOperationException("This method may not be called when analytics transmission is disabled.");
+            }
+
+            Console.WriteLine(@"Simulating transmission of {0}", analyticsEvent);
+        }
+    }
+}
