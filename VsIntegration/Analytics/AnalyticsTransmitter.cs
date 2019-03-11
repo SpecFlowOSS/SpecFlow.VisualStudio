9f36474c3e3ba68f0b229273524dee4d2ab3fca6
David Eiwen <david-e-winlive@live.at>
2019-03-11 10:22:42 +0100

    catch all exceptions in TransmitExtensionLoadedEvent

--- VsIntegration/Analytics/AnalyticsTransmitter.cs
+++ VsIntegration/Analytics/AnalyticsTransmitter.cs
@@ -18,14 +18,21 @@ namespace TechTalk.SpecFlow.VsIntegration.Analytics
 
         public void TransmitExtensionLoadedEvent(string ide, string ideVersion, string extensionVersion)
         {
-            if (!_enableAnalyticsChecker.IsEnabled())
+            try
             {
-                return;
+                if (!_enableAnalyticsChecker.IsEnabled())
+                {
+                    return;
+                }
+
+                var userUniqueId = _userUniqueIdStore.Get();
+                var logonAnalyticsEvent = new ExtensionLoadedAnalyticsEvent(DateTime.UtcNow, userUniqueId, ide, ideVersion, extensionVersion);
+                _analyticsTransmitterSink.TransmitEvent(logonAnalyticsEvent);
             }
-            
-            var userUniqueId = _userUniqueIdStore.Get();
-            var logonAnalyticsEvent = new ExtensionLoadedAnalyticsEvent(DateTime.UtcNow, userUniqueId, ide, ideVersion, extensionVersion);
-            _analyticsTransmitterSink.TransmitEvent(logonAnalyticsEvent);
+            catch (Exception)
+            {
+                // catch all exceptions since we do not want to break the whole extension simply because data transmission failed
+            }
         }
     }
 }
