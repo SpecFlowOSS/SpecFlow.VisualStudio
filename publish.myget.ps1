c5597c9ca11978a75eb57bce58a9a8893b674f8f
Andreas Willich <andreas.willich@techtalk.at>
2019-03-14 14:00:22 +0100

    fix myget script

--- publish.myget.ps1
+++ publish.myget.ps1
@@ -6,6 +6,11 @@ $VisualStudioVersion = $Env:VisualStudioVersion;
 
 $pathToExtension = "$SystemArtifactsDirectory\SpecFlow.VisualStudio.VS$VisualStudioVersion\s\VsIntegration\bin\Release\TechTalk.SpecFlow.VsIntegration.$VisualStudioVersion.vsix";
 
+if (![System.File]::Exists($pathToExtension))
+{
+    $pathToExtension = "$SystemArtifactsDirectory\SpecFlow.VisualStudio.VS$VisualStudioVersion\s\VsIntegration\bin\Release\TechTalk.SpecFlow.VisualStudioIntegration.vsix";
+}
+
 $extensionFileContent = [System.IO.File]::ReadAllBytes("$pathToExtension");
 
 & Invoke-WebRequest -Uri "$MyGetVsixFeed/upload" -Method 'POST' -Body $extensionFileContent -Headers @{"X-NuGet-ApiKey"="$MyGetApiKey"}
