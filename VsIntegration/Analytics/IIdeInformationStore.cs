86507845c9e6e65a4637f28880bd6d8c495a28f8
David Eiwen <david-e-winlive@live.at>
2019-03-19 16:28:25 +0100

    add actual app insights transmission (#50)

---
+++ VsIntegration/Analytics/IIdeInformationStore.cs
@@ -0,0 +1,9 @@
+﻿namespace TechTalk.SpecFlow.VsIntegration.Analytics
+{
+    public interface IIdeInformationStore
+    {
+        string GetName();
+
+        string GetVersion();
+    }
+}
---
+++ VsIntegration/Analytics/VisualStudioIdeInformationStore.cs
@@ -0,0 +1,19 @@
+﻿using TechTalk.SpecFlow.VsIntegration.Utils;
+
+namespace TechTalk.SpecFlow.VsIntegration.Analytics
+{
+    public class VisualStudioIdeInformationStore : IIdeInformationStore
+    {
+        private const string IdeName = "Microsoft Visual Studio";
+
+        public string GetName()
+        {
+            return IdeName;
+        }
+
+        public string GetVersion()
+        {
+            return VSVersion.FullVersion.ToString();
+        }
+    }
+}
