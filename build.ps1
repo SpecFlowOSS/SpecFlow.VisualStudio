19a766d283a152f4c7a882628acfbbba476cb6e4
Andreas Willich <andreas.willich@techtalk.at>
2019-03-19 21:21:34 -0700

    work on build.ps1

--- build.ps1
+++ build.ps1
@@ -4,6 +4,8 @@ param (
 
 $msbuildPath = "msbuild"
 
+Write-Host $IsWindows
+
 if ($IsWindows){
   $vswherePath = [System.Environment]::ExpandEnvironmentVariables("%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe")
   $vswhereParameters = @("-latest", "-products", "*", "-requires", "Microsoft.Component.MSBuild",  "-property", "installationPath")
