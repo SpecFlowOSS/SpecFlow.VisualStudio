b6e407522fa0ebfd59b1345ae57e7fa72bd3b655
Andreas Willich <andreas.willich@techtalk.at>
2019-03-19 17:39:34 -0700

    adjust azure pipeline

--- build.ps1
+++ build.ps1
@@ -13,16 +13,6 @@ if (![System.String]::IsNullOrEmpty($Env:MSBuild))
   $msbuildPath = join-path $Env:MSBuild 'msbuild.exe';
   Write-Host "Using msbuild from environment variable";
 }
-elseif ($SpecFlowVisualStudioVersion -eq "2017")
-{
-  $msbuildPath = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe";
-  $binaryLoggerSwitch = "/binaryLogger:msbuild.$Configuration.binlog";
-}
-elseif ($SpecFlowVisualStudioVersion -eq "2015")
-{
-  $msbuildPath = "C:\Program Files (x86)\MSBuild\14.0\bin\MSBuild.exe";
-  $binaryLoggerSwitch = "";
-}
 elseif ($SpecFlowVisualStudioVersion -eq "2019")
 {
   $msbuildPath = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Preview\MSBuild\Current\Bin\MSBuild.exe";
