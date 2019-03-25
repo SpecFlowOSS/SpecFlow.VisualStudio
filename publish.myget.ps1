1fcc05e2c125625372add22126f969ea3ad6a57a
Andreas Willich <andreas.willich@techtalk.at>
2019-03-25 09:49:00 +0100

    add logging to publish script

--- publish.myget.ps1
+++ publish.myget.ps1
@@ -7,8 +7,12 @@ Get-ChildItem -Path $SystemArtifactsDirectory -Filter *.vsix |
 ForEach-Object {
     $filename = Get-Content $_.FullName
 
+	Write-Host "Uploading file $filename"
+
     $extensionFileContent = [System.IO.File]::ReadAllBytes("$filename");
 
     & Invoke-WebRequest -Uri "$MyGetVsixFeed/upload" -Method 'POST' -Body $extensionFileContent -Headers @{"X-NuGet-ApiKey"="$MyGetApiKey"}
+
+    Write-Host "File was uploaded"
 }
 
