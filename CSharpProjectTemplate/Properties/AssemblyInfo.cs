3ae9930fa38e928068cd65bcbb3508d941f79640
Balazs Epresi <epresib@gmail.com>
2020-06-26 15:20:00 +0200

    Spike: try out C# Project Template

---
+++ CSharpProjectTemplate/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SpecFlow.CSharpProjectTemplate")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Balazs Epresi")]
+[assembly: AssemblyProduct("CSharpProjectTemplate")]
+[assembly: AssemblyCopyright("Copyright © Balazs Epresi 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7814b082-04a0-4210-9add-c5f01341acd4")]
+
---
+++ VsIntegration/WizardImplementation.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+
+namespace TechTalk.SpecFlow.VsIntegration
+{
+    public class WizardImplementation : IWizard
+    {
+        private UserInputForm inputForm;
+        private string customMessage;
+
+        // This method is called before opening any item that
+        // has the OpenInEditor attribute.
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+        }
+
+        // This method is only called for item templates,
+        // not for project templates.
+        public void ProjectItemFinishedGenerating(ProjectItem
+            projectItem)
+        {
+        }
+
+        // This method is called after the project is created.
+        public void RunFinished()
+        {
+        }
+
+        public void RunStarted(object automationObject,
+            Dictionary<string, string> replacementsDictionary,
+            WizardRunKind runKind, object[] customParams)
+        {
+            try
+            {
+                // Display a form to the user. The form collects
+                // input for the custom message.
+                inputForm = new UserInputForm();
+                inputForm.ShowDialog();
+
+                customMessage = UserInputForm.CustomMessage;
+
+                // Add custom parameters.
+                replacementsDictionary.Add("$custommessage$",
+                                           customMessage);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        // This method is only called for item templates,
+        // not for project templates.
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return true;
+        }
+    }
+
+    public partial class UserInputForm : Form
+    {
+        private static string customMessage;
+        private TextBox textBox1;
+        private Button button1;
+
+        public UserInputForm()
+        {
+            this.Size = new System.Drawing.Size(155, 265);
+
+            button1 = new Button();
+            button1.Location = new System.Drawing.Point(90, 25);
+            button1.Size = new System.Drawing.Size(50, 25);
+            button1.Click += button1_Click;
+            this.Controls.Add(button1);
+
+            textBox1 = new TextBox();
+            textBox1.Location = new System.Drawing.Point(10, 25);
+            textBox1.Size = new System.Drawing.Size(70, 20);
+            this.Controls.Add(textBox1);
+        }
+        public static string CustomMessage
+        {
+            get
+            {
+                return customMessage;
+            }
+            set
+            {
+                customMessage = value;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            customMessage = textBox1.Text;
+            this.Close();
+        }
+    }
+}
