086cfdda14dd9b38958f92557ba8ae6ac8bc3956
Zsolt Olah <z.olah@tricentis.com>
2020-07-08 16:36:52 +0200

    Implement the wizard using WPF - #8167

---
+++ ProjectTemplateWizard/UserInputControl.xaml.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ProjectTemplateWizard
+{
+    /// <summary>
+    /// Interaction logic for UserInputControl.xaml
+    /// </summary>
+    public partial class UserInputControl
+    {
+        public UserInputControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- ProjectTemplateWizard/WizardImplementation.cs
+++ ProjectTemplateWizard/WizardImplementation.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
-using System.Windows.Forms;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
 using EnvDTE;
+using Microsoft.VisualStudio.PlatformUI;
+using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.TemplateWizard;
 
 namespace ProjectTemplateWizard
 {
     public class WizardImplementation : IWizard
     {
-        private UserInputForm _inputForm;
+        private UserInputDialog _inputDialog;
+        private string _projectDirectory;
+        private string _solutionDirectory;
 
         // This method is called before opening any item that
         // has the OpenInEditor attribute.
@@ -39,18 +45,30 @@ namespace ProjectTemplateWizard
         {
             try
             {
-                // Display a form to the user. The form collects
-                // input for the custom message.
-                _inputForm = new UserInputForm();
-                _inputForm.ShowDialog();
+                _projectDirectory = replacementsDictionary["$destinationdirectory$"];
+                _solutionDirectory = replacementsDictionary["$solutiondirectory$"];
 
-                // Add custom parameters.
-                replacementsDictionary.Add("$targetframework$", UserInputForm.TargetFramework);
-                replacementsDictionary.Add("$unittestprovider$", UserInputForm.UnitTestProvider);
+                _inputDialog = new UserInputDialog();
+                bool? dialogResult = _inputDialog.ShowDialog();
+                if (dialogResult.HasValue && dialogResult.Value)
+                {
+                    // Add custom parameters.
+                    replacementsDictionary.Add("$dotnetframework$", _inputDialog.DotNetFramework);
+                    replacementsDictionary.Add("$unittestframework$", _inputDialog.UnitTestFramework);
+                }
+                else
+                {
+                    CancelProjectCreation();
+                }
+            }
+            catch (WizardBackoutException)
+            {
+                throw;
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.ToString());
+                CancelProjectCreation();
             }
         }
 
@@ -60,79 +78,76 @@ namespace ProjectTemplateWizard
         {
             return true;
         }
-    }
 
-    class ComboBoxItem
-    {
-        public string Value { get; set; }
-        public string Text { get; set; }
-
-        public override string ToString() => Text;
-    }
-
-    public class UserInputForm : Form
-    {
-        private readonly ComboBox _targetFrameWorkComboBox;
-        private readonly ComboBox _unitTestProviderWorkComboBox;
-        private readonly Button _createButton;
-
-        public static string TargetFramework { get; private set; }
-        public static string UnitTestProvider { get; private set; }
-
-        public UserInputForm()
+        private void CancelProjectCreation()
         {
-            Size = new Size(640, 480);
+            Cleanup();
 
-            var dotNetCoreComboBoxItem = new ComboBoxItem { Value = "netcoreapp3.1", Text = ".Net Core 3.1" };
-            var fullFrameworkComboBoxItem = new ComboBoxItem { Value = "net472", Text = ".Net Framework 4.7.2" };
-
-            _targetFrameWorkComboBox = new ComboBox
-            {
-                Location = new Point(10, 25),
-                Size = new Size(150, 25),
-                DataSource = new[]
-                {
-                    dotNetCoreComboBoxItem,
-                    fullFrameworkComboBoxItem
-                },
-                SelectedItem = dotNetCoreComboBoxItem
-            };
-            Controls.Add(_targetFrameWorkComboBox);
-
-            var runnerComboBoxItem = new ComboBoxItem { Value = "runner", Text = "SpecFlow+ Runner"};
-            var xUnitComboBoxItem = new ComboBoxItem { Value = "xunit", Text = "xUnit" };
-            var nUnitComboBoxItem = new ComboBoxItem { Value = "nunit", Text = "NUnit" };
-            var msTestComboBoxItem = new ComboBoxItem { Value = "mstest", Text = "MSTest" };
-
-            _unitTestProviderWorkComboBox = new ComboBox()
-            {
-                Location = new Point(10, 75),
-                Size = new Size(150, 25),
-                DataSource = new[]
-                {
-                    runnerComboBoxItem,
-                    xUnitComboBoxItem,
-                    nUnitComboBoxItem,
-                    msTestComboBoxItem
-                },
-                SelectedItem = runnerComboBoxItem
-            };
-            Controls.Add(_unitTestProviderWorkComboBox);
-
-            _createButton = new Button
-            {
-                Location = new Point(10, 125),
-                Size = new Size(50, 25),
-                Text = "Create"
-            };
-            _createButton.Click += CreateButtonClick;
-            Controls.Add(_createButton);
+            // Cancel the project creation.
+            throw new WizardBackoutException();
         }
-        
-        private void CreateButtonClick(object sender, EventArgs e)
+
+        private void Cleanup()
         {
-            TargetFramework = (_targetFrameWorkComboBox.SelectedValue as ComboBoxItem)?.Value;
-            UnitTestProvider = (_unitTestProviderWorkComboBox.SelectedValue as ComboBoxItem)?.Value;
+            if (Directory.Exists(_projectDirectory))
+            {
+                Directory.Delete(_projectDirectory, true);
+            }
+
+            if (_projectDirectory != _solutionDirectory &&
+                Directory.Exists(_solutionDirectory) &&
+                !Directory.EnumerateFileSystemEntries(_solutionDirectory).Any())
+            {
+                Directory.Delete(_solutionDirectory);
+            }
+        }
+    }
+
+    public class UserInputDialog : DialogWindow
+    {
+        readonly UserInputControl _userInputControl;
+        
+        public string DotNetFramework { get; private set; }
+        public string UnitTestFramework { get; private set; }
+
+        private static SolidColorBrush ToBrush(ThemeResourceKey key)
+        {
+            var color = VSColorTheme.GetThemedColor(key);
+            return new SolidColorBrush(Color.FromArgb(color.A, color.R, color.G, color.B));
+        }
+
+        internal UserInputDialog()
+        {
+            IsCloseButtonEnabled = true;
+
+            _userInputControl = new UserInputControl();
+
+            MinWidth = _userInputControl.MinWidth;
+            MinHeight = _userInputControl.MinHeight;
+            Width = _userInputControl.Width;
+            Height = _userInputControl.Height;
+
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            Content = _userInputControl.Content;
+            _userInputControl.BackButton.Click += CloseDialog;
+            _userInputControl.CreateButton.Click += SetCustomParametersAndCloseDialog;
+
+            Background = ToBrush(EnvironmentColors.ToolboxBackgroundColorKey);
+        }
+
+        private void CloseDialog(object sender, EventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+
+        private void SetCustomParametersAndCloseDialog(object sender, EventArgs e)
+        {
+            DotNetFramework = _userInputControl.DotNetFrameworkComboBox.SelectedValue as string;
+            UnitTestFramework = _userInputControl.UnitTestFrameworkComboBox.SelectedValue as string;
+
+            DialogResult = true;
             Close();
         }
     }
