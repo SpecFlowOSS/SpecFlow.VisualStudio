629e73ccda8749d048659850e28bbbf4badd8f02
Zsolt Olah <z.olah@tricentis.com>
2020-07-15 15:16:58 +0200

    Make adjustments according to PR code review comments - #8307
    - Move public class "UserInputDialog" to its own file
    - Adjust indentation and spacing in the csproj file in the project template to improve code readability
    - Use the already existing key file to sign the ProjectTemplateWizard assembly

---
+++ ProjectTemplateWizard/UserInputDialog.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shell;
+using Microsoft.VisualStudio.PlatformUI;
+using Microsoft.VisualStudio.Shell;
+
+namespace ProjectTemplateWizard
+{
+    public class UserInputDialog : DialogWindow
+    {
+        readonly UserInputControl _userInputControl;
+        
+        public string DotNetFramework { get; private set; }
+        public string UnitTestFramework { get; private set; }
+
+        private static SolidColorBrush ToBrush(ThemeResourceKey key)
+        {
+            var color = VSColorTheme.GetThemedColor(key);
+            return new SolidColorBrush(Color.FromArgb(color.A, color.R, color.G, color.B));
+        }
+
+        internal UserInputDialog()
+        {
+            _userInputControl = new UserInputControl();
+
+            MinWidth = _userInputControl.MinWidth;
+            MinHeight = _userInputControl.MinHeight;
+            Width = _userInputControl.Width;
+            Height = _userInputControl.Height;
+
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            Content = _userInputControl.Content;
+            _userInputControl.CloseButton.Click += CloseDialog;
+            _userInputControl.BackButton.Click += CloseDialog;
+            _userInputControl.CreateButton.Click += SetCustomParametersAndCloseDialog;
+
+            WindowStyle = WindowStyle.None;
+            WindowChrome.SetWindowChrome(this, new WindowChrome
+            {
+                CaptionHeight = 32,
+                ResizeBorderThickness = SystemParameters.WindowResizeBorderThickness
+            });
+            Background = ToBrush(EnvironmentColors.ToolboxBackgroundColorKey);
+        }
+
+        private void CloseDialog(object sender, EventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+
+        private void SetCustomParametersAndCloseDialog(object sender, EventArgs e)
+        {
+            DotNetFramework = _userInputControl.DotNetFrameworkComboBox.SelectedValue as string;
+            UnitTestFramework = _userInputControl.UnitTestFrameworkComboBox.SelectedValue as string;
+
+            DialogResult = true;
+            Close();
+        }
+    }
+}
