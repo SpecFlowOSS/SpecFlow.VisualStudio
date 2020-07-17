d10f3a38eac97a84e664b292b94796647712d005
tawfiknouri <nouri.tawfik@gmail.com>
2020-07-17 12:42:08 +0200

    Improvement of the default specflow example

--- ItemTemplates/SpecFlowStepDefinition/SpecFlowStepDefinition1.fs
+++ ItemTemplates/SpecFlowStepDefinition/SpecFlowStepDefinition1.fs
@@ -5,11 +5,14 @@ open TechTalk.SpecFlow
 
 // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
 
-let [<Given>] ``I have entered (.*) into the calculator``(number:int) = 
+let [<Given>] ``the first number is (.*)``(number:int) = 
     ScenarioContext.StepIsPending() //TODO: implement arrange (precondition) logic
 
-let [<When>] ``I press add``() = 
+let [<Given>] ``the second number is (.*)``(number:int) = 
+    ScenarioContext.StepIsPending() //TODO: implement arrange (precondition) logic
+
+let [<When>] ``the two numbers are added``() = 
     ScenarioContext.StepIsPending() //TODO: implement act (action) logic
 
-let [<Then>] ``the result should be (.*) on the screen``(result:int) = 
+let [<Then>] ``the result should be (.*)``(result:int) = 
     ScenarioContext.StepIsPending() //TODO: implement assert (verification) logic
