5fa7bdc489334237ecfc1f05e2f6c6fe365dc00b
Balázs Epresi <epresib@gmail.com>
2020-12-09 09:27:01 +0100

    Fix: Renaming a step should not change the parameters (#102)
    * Fix: Renaming a step should not change the parameters
    
    * Update wizard package versions

---
+++ TechTalk.SpecFlow.VsIntegration.Implementation/EditorCommands/IStepNameReplacer.cs
@@ -0,0 +1,9 @@
+﻿using TechTalk.SpecFlow.Bindings;
+
+namespace TechTalk.SpecFlow.VsIntegration.Implementation.EditorCommands
+{
+    public interface IStepNameReplacer
+    {
+        public string BuildStepNameWithNewRegex(string stepName, string newStepRegex, IStepDefinitionBinding binding);
+    }
+}
