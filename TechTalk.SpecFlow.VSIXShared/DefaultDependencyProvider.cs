1f2b55af6548f74c886dc336ed715bd61f0710af
Balázs Epresi <epresib@gmail.com>
2021-07-02 14:20:50 +0200

    Solve mergecommits of rebase

--- TechTalk.SpecFlow.VSIXShared/DefaultDependencyProvider.cs
+++ TechTalk.SpecFlow.VSIXShared/DefaultDependencyProvider.cs
@@ -14,7 +14,6 @@ using TechTalk.SpecFlow.VsIntegration.Implementation.Commands;
 using TechTalk.SpecFlow.VsIntegration.Implementation.EditorCommands;
 using TechTalk.SpecFlow.VsIntegration.Implementation.Install;
 using TechTalk.SpecFlow.VsIntegration.Implementation.LanguageService;
-using TechTalk.SpecFlow.VsIntegration.Implementation.Services;
 using TechTalk.SpecFlow.VsIntegration.Implementation.Notifications;
 using TechTalk.SpecFlow.VsIntegration.Implementation.Tracing;
 using TechTalk.SpecFlow.VsIntegration.Implementation.Tracing.OutputWindow;
